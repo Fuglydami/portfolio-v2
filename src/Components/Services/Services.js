df9379454ec53c76f3b116ab1537bb54d0dc50e6
Damilare Oyedeji <damoye81@gmail.com>
2024-08-01 16:58:14 +0100

    update

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,8 +1,8 @@
-import { UilArrow, UilDatabase } from "@iconscout/react-unicons";
-import React from "react";
-import { Helmet } from "react-helmet-async";
-import "./Services.css";
-import ServicesContent from "./ServicesContent";
+import { UilArrow, UilDatabase } from '@iconscout/react-unicons';
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import './Services.css';
+import ServicesContent from './ServicesContent';
 
 function Services({ services }) {
   return (
@@ -10,26 +10,26 @@ function Services({ services }) {
       <Helmet>
         <title>Damilare Oyedeji - Software Engineer</title>
         <meta
-          name="description"
-          content="Very vast in these technologies; JavaScript, TypeScript, ReactJs, React Native, BitBucket, Jira, Azure, Git/Github, Styled-Component, Tailwind CSS, Node, MongoDB..."
+          name='description'
+          content='Very vast in these technologies; JavaScript, TypeScript, ReactJs, React Native, BitBucket, Jira, Azure, Git/Github, Styled-Component, Tailwind CSS, Node, MongoDB...'
         />
-        <link rel="canonical" href="/#services" />
+        <link rel='canonical' href='/#services' />
       </Helmet>
-      <section className="services section" id="services">
-        <h2 className="section__title">What i do</h2>
-        <span className="section__subtitle">
+      <section className='services section' id='services'>
+        <h2 className='section__title'>What i do</h2>
+        <span className='section__subtitle'>
           What i offer/Technologies I use
         </span>
 
-        <div className="services__container container grid">
+        <div className='services__container container grid'>
           <ServicesContent
             Icon={UilArrow}
-            title="Frontend Developer"
+            title='Frontend Development'
             services={services}
           />
           <ServicesContent
             Icon={UilDatabase}
-            title="Backend Developer"
+            title='Backend Development'
             services={services}
           />
         </div>
